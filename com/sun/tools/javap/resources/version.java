1f7359ec2858d96008b1d5e9dba899157d5c7913
Lucas H. Xu <xuhang57@users.noreply.github.com>
2019-12-03 12:43:43 -0500

    Add JDK-8 From src.zip

---
+++ com/sun/tools/javap/resources/version.java
@@ -0,0 +1,11 @@
+package com.sun.tools.javap.resources;
+
+public final class version extends java.util.ListResourceBundle {
+    protected final Object[][] getContents() {
+        return new Object[][] {
+            { "full", "1.8.0_222-8u222-b10-1ubuntu1~18.04.1-b10" },
+            { "jdk", "1.8.0_222" },
+            { "release", "1.8.0_222" },
+        };
+    }
+}
